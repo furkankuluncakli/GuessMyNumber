1a91dfcdb26c9f72b6a93d06116e2f0ed43cac58
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-06-10 14:02:23 +0300

    feat: enhance StartGameScreen TextInput with keyboard settings

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -4,7 +4,13 @@ import { StyleSheet, TextInput, View } from "react-native";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput style={styles.numberInput} maxLength={2}
+      keyboardType="number-pad"
+      autoCorrect={false}
+      autoCapitalize="none"
+
+      
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
