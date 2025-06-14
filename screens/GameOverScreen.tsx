e027448718d46b7f1af986e798b579a4de7c5a06
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-06-14 16:46:48 +0300

    feat: enhance GameOverScreen layout with Title and success image

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,12 +1,40 @@
+import Title from '@/components/ui/Title'
+import { Colors } from '@/constants/Colors'
 import React from 'react'
-import { Text } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { Image, View } from 'react-native'
 
 function GameOverScreen() {
   return (
-    <Text>
-        Game Is OVER!
-    </Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Is OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/images/success.png")}/>
+      </View>
+    </View>
   )
 }
 
 export default GameOverScreen
+
+const styles = StyleSheet.create({
+  rootContainer:{
+    flex:1,
+    padding:24,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  imageContainer:{
+    height:300,
+    width:300,
+    borderRadius:150,
+    borderWidth:2,
+    borderColor:Colors.accent500,
+    overflow:"hidden",
+    margin:36
+  },
+  image:{
+    width:"100%",
+    height:"100%"
+  }
+})
